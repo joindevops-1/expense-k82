5428a81aa8043af2676c6cd5d4a28da6446f5d9d
sivakumarreddym <info@joindevops.com>
2025-03-17 17:52:17 +0530

    expense infra

--- nginx/helm.sh
+++ nginx/helm.sh
@@ -4,7 +4,7 @@ CURRENT_VERSION=""
 if ! kubectl get configmap blue-green-status -n expense > /dev/null 2>&1; then
     echo "First deployment detected! Creating ConfigMap and deploying Blue..."
     kubectl create configmap blue-green-status --from-literal=live-version="blue"
-    NEW_VERSION="blue"
+    PREVIEW_VERSION="blue"
 else
     CURRENT_VERSION=$(kubectl get configmap blue-green-status -o=jsonpath='{.data.live-version}')
     echo "Current Version: $CURRENT_VERSION"
