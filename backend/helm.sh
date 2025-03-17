2981f46f4e832d3bf7e4bc4322d31990750c3b1c
sivakumarreddym <info@joindevops.com>
2025-03-17 17:36:00 +0530

    expense infra

--- backend/helm.sh
+++ backend/helm.sh
@@ -9,11 +9,12 @@ else
     CURRENT_VERSION=$(kubectl get configmap blue-green-status -o=jsonpath='{.data.live-version}')
     echo "Current Version: $CURRENT_VERSION"
     if [ "$CURRENT_VERSION" == "blue" ]; then
-        NEW_VERSION="green"
+        PREVIEW_VERSION="green"
     else
-        NEW_VERSION="blue"
+        #NEW_VERSION="blue"
+        PREVIEW_VERSION="blue"
     fi
 fi
 
-helm upgrade --install backend . --set service.currentVersion=$NEW_VERSION
+helm upgrade --install backend . --set service.previewVersion=$NEW_VERSION
 kubectl patch configmap blue-green-status -n expense --type merge -p '{"data":{"live-version":"'$NEW_VERSION'"}}'
